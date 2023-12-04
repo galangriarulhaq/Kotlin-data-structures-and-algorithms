30f2e36d798a1ebd2d8cb4ea57f11641567fdc5a
galangriarulhaq <galangrz337@gmail.com>
2023-12-04 21:20:08 +0700

    Update-algorithms-recursion

---
+++ src/main/kotlin/algorithms/recursion/reverse_string/ReverseString.kt
@@ -0,0 +1,43 @@
+package algorithms.recursion.reverse_string
+
+class ReverseString {
+
+    /**
+     * Returns the reverse of the given string using a recursive approach.
+     *
+     * @param str the string to reverse
+     * @return the reverse of the given string
+     */
+    fun reverseStringRecursive(str: String): String {
+        return if (str == "")
+            ""
+        else
+            reverseStringRecursive(str.substring(1)) + str[0]
+    }
+
+    /**
+     * Returns the reverse of the given string using an iterative approach.
+     *
+     * @param str the string to reverse
+     * @return the reverse of the given string
+     */
+    fun reverseStringIterative(str: String): String {
+        val charArray = str.toCharArray()
+        for (i in 0 until charArray.size / 2) {
+            val temp = charArray[i]
+            charArray[i] = charArray[charArray.size - 1 - i]
+            charArray[charArray.size - 1 - i] = temp
+        }
+        return String(charArray)
+    }
+}
+
+fun main() {
+    val reverse = ReverseString()
+    val recursive = reverse.reverseStringRecursive("Galang")
+    val iterative = reverse.reverseStringIterative("Riarulhaq")
+    println(recursive)
+    println(iterative)
+    println(reverse.reverseStringRecursive(recursive))
+    println(reverse.reverseStringRecursive(iterative))
+}
