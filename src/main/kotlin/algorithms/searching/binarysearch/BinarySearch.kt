ee03fcac9bf005ab95457708bdaa43721af53cd6
galangriarulhaq <galangrz337@gmail.com>
2023-12-05 16:41:54 +0700

    Add-algorithms-searching-binarySearch

---
+++ src/main/kotlin/algorithms/searching/binarysearch/BinarySearch.kt
@@ -0,0 +1,45 @@
+package algorithms.searching.binarysearch
+
+class BinarySearch {
+
+    /**
+     * Performs a binary search on an array of integers to find the index of a specified key.
+     *
+     * @param arr the array to search
+     * @param key the value to search for
+     * @return a message indicating whether the element was found and its index, or that it was not found
+     */
+    fun binarySearch(arr: IntArray, key: Int): String {
+        var first = 0
+        var last = arr.size - 1
+        var middle = (first + last) / 2
+        while (first <= last) {
+            when {
+                arr[middle] < key -> {
+                    first = middle + 1
+                }
+                arr[middle] == key -> {
+                    return "Element $key found at index $middle"
+                }
+                else -> {
+                    last = middle - 1
+                }
+            }
+            middle = (first + last) / 2
+        }
+        return "Element $key not found!"
+    }
+}
+
+fun main() {
+
+    val binarySearch = BinarySearch()
+
+    // Note: Binary search works only with sorted arrays.
+    val array = intArrayOf(10, 25, 32, 45, 55, 68)
+    val result = binarySearch.binarySearch(array, 55)
+    println(result)
+
+    val result2 = binarySearch.binarySearch(array, 100)
+    println(result2)
+}
