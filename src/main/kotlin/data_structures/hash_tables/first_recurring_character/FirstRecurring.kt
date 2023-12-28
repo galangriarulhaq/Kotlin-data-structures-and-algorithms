5a68f3161274e560df83034e14ca04fedef5d78e
galangriarulhaq <galangrz337@gmail.com>
2023-12-28 23:16:28 +0700

    Update-datastructures-Add-hash_tables-first_recurring_character

---
+++ src/main/kotlin/data_structures/hash_tables/first_recurring_character/FirstRecurring.kt
@@ -0,0 +1,24 @@
+package data_structures.hash_tables.first_recurring_character
+
+/**
+ * Finds the first recurring integer in an array of integers.
+ * @param array an integer array to search for recurring integer
+ * @return The first recurring integer or null if none found
+ */
+private fun firstRecurringCharacter(array: IntArray): Int? {
+    val hashSet = HashSet<Int>()
+    for (element in array) {
+        if (hashSet.contains(element)) {
+            return element
+        } else {
+            hashSet.add(element)
+        }
+    }
+    return null
+}
+
+fun main() {
+
+    val firstRecurring = firstRecurringCharacter(intArrayOf(2, 2, 2, 5, 5, 2, 3, 5, 1, 2, 4))
+    println(firstRecurring)
+}
